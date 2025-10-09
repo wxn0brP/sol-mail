7171850bd2f698ac3f08e1a15063dddb73a0e72c
wxn0brP <toja.filiper@gmail.com>
2025-10-09 16:27:29 +0200

    feat: update upload files

--- front/src/pages/upload.ts
+++ front/src/pages/upload.ts
@@ -7,9 +7,49 @@ checkTokenRefresh();
 const uploadForm = qs("form");
 
 const fileInput = qi("#filefield");
+const filesList = qs("#filesList");
 const mailNameInput = qi("#mailName");
 const bodyInput = qi("#body");
 const message = qs("#message");
+
+let selectedFiles: File[] = [];
+fileInput.addEventListener("change", async (event) => {
+    const files = (event.target as HTMLInputElement).files;
+    if (files) {
+        for (let i = 0; i < files.length; i++) {
+            const existsIndex = selectedFiles.findIndex(f => f.name === files[i].name);
+            if (existsIndex !== -1) {
+                const conf = await confirm(t("A file '$' with the same name already exists. Do you want to replace it").replace("$", files[i].name) + "?");
+                if (conf) continue;
+                selectedFiles.splice(existsIndex, 1);
+            }
+            selectedFiles.push(files[i]);
+        }
+        updateFilesList();
+    }
+    (event.target as HTMLInputElement).value = "";
+});
+
+function updateFilesList() {
+    filesList.innerHTML = "";
+    selectedFiles.forEach((file, index) => {
+        const li = document.createElement("li");
+
+        const fileNameSpan = document.createElement("span");
+        fileNameSpan.innerHTML = file.name;
+
+        const removeBtn = document.createElement("button");
+        removeBtn.innerHTML = "X";
+        removeBtn.addEventListener("click", () => {
+            selectedFiles.splice(index, 1);
+            updateFilesList();
+        });
+
+        li.appendChild(fileNameSpan);
+        li.appendChild(removeBtn);
+        filesList.appendChild(li);
+    });
+}
 
 uploadForm.addEventListener("submit", async (event) => {
     event.preventDefault();
@@ -21,7 +61,7 @@ uploadForm.addEventListener("submit", async (event) => {
     }
     const body = bodyInput.value.trim();
 
-    if (fileInput.files.length === 0) {
+    if (selectedFiles.length === 0) {
         const conf = await confirm(t("Are you sure you want to upload an empty file") + "?");
         if (!conf) return;
     }
@@ -31,7 +71,7 @@ uploadForm.addEventListener("submit", async (event) => {
     if (body)
         formData.append("body", body);
 
-    for (const file of fileInput.files) {
+    for (const file of selectedFiles) {
         formData.append("filefield", file);
     }
 
@@ -48,6 +88,7 @@ uploadForm.addEventListener("submit", async (event) => {
         message.attrib("translate", "Upload finished");
         mailNameInput.value = "";
         bodyInput.value = "";
-        fileInput.value = "";
+        selectedFiles = [];
+        updateFilesList();
     }
 });
