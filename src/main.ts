054c08da2d5a53fb8d5732441549f439ebd3fd84
wxn0brP <toja.filiper@gmail.com>
2026-03-21 11:57:08 +0100

    chore: bump deps

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ if (!process.env.JWT_SECRET) {
 }
 
 const app = new FalconFrame();
-app.setVar("layout", "public/layout.html");
 
 app.get("/server.find", (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
